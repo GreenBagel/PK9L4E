54ef65cf3ff32ea7189f9ca0a06bf3265a1af8ad
ben2x5 <benyamincahyai@gmail.com>
2014-12-02 15:44:43 +0800

    this is my front end

---
+++ FrontEnd/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="text-align: center">Welcome to Reserve Flight Tickets Website</h1>
+        <a href="searchFlight.php">Search flight</a>
+        <a href="reserveFlight.php">Book now</a>
+        <a href="viewPurchase.php">View Purchase Detail</a>
+    </body>
+</html>
---
+++ FrontEnd/searchFlight.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <form action="" method="post">
+            <h2>Search Flight</h2>
+            Date:
+            <input type="date" name="deptDate">
+            Origin:
+            <input type="text" name="originCity">
+            Destination:
+            <input type="text" name ="destCity">
+            <input type="submit" value="submit"> 
+        </form>
+    </body>
+</html>
