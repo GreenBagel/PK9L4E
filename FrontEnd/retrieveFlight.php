197cc5a696ab8ff7df1a3fa47d773450faa71852
ben2x5 <benyamincahyai@gmail.com>
2014-12-18 11:30:53 +0800

    retrieve page changed table

--- FrontEnd/retrieveFlight.php
+++ FrontEnd/retrieveFlight.php
@@ -11,98 +11,100 @@
 
     <body>
         <div id="container">
-        <div id="header"><a href="../index.php"><h3>Airline Reservation System<img id="home-icon" src="../lib/image/home-icon.png" alt="Home"></h3></a></div>
-        <div id="body">
-        <div id="clockbox"></div>
-        <h2>Retrieve flight</h2>
+            <div id="header"><a href="../index.php"><h3>Airline Reservation System<img id="home-icon" src="../lib/image/home-icon.png" alt="Home"></h3></a></div>
+            <div id="body">
+                <div id="clockbox"></div>
+                <h2>Retrieve flight</h2>
 
-        <!-- PHP -->
-        <?php
-        require_once('../database/database.php');
-        require_once('../database/locations.php');
-        require_once('../database/flights.php');
-        require_once('../database/customers.php');
-        require_once('../database/reservations.php');
-
-        $database = new Database('root', '', 'sqm_fp');
-        $locations = new Locations($database);
-        $flights = new Flights($database);
-        $customers = new Customers($database);
-        $res = new Reservations($database);
-        ?>
-
-        <!-- Form -->
-        <div id="form-wrapper">
-            <form class="horizontal-form" role="form" name="myForm" action="retrieveFlight.php" onsubmit="javascript:return validate();" method="post">
-                <div class="form-group">
-                <label class="col-sm-4 control-label">Reservation Code: &nbsp </label>
-                <div class="col-sm-4">
-                <input class="form-control" type="text" name="code"> &nbsp
-                </div> 
-                </div>
-                <input class="btn btn-primary" type="submit" value="Submit">
-            </form>
-        </div>
-
-        <table border="1" class="table table-bordered">
-
-            <!-- Flight Code -->
-            <tr>
-                <th align="center">Flight code</th>
-                <th align="center">Origin</th>
-                <th align="center">Departure Date & Time</th>
-                <th align="center">Seat Number </th>
-                <th align="center">Name</th>
-                <th align="center">NRIC</th>
-                <th align="center">Contact Number</th>
-                <th align="center">Email Address</th>
-
-            </tr>
-
-            <?php
-            $reservation_code = (isset($_POST['code']) ? $_POST['code'] : null);
-            if ($reservation_code == null) {
-                ?> 
-                <tr> 
-                    <td  colspan ="8" align="center">Flight information will be diplayed here.</td>
-                </tr>             
+                <!-- PHP -->
                 <?php
-            } else {
-                try {
-                    $resInfo = $res->GetReservationWithFieldsFilter($reservation_code, null, null, null, null, null, null, null, null, null);
-                    //$resInfo2 = $flights->GetFlightWithFieldsFilter($result[1], null, null, null, null, null, null, null, null);
+                require_once('../database/database.php');
+                require_once('../database/locations.php');
+                require_once('../database/flights.php');
+                require_once('../database/customers.php');
+                require_once('../database/reservations.php');
 
-                    foreach ($resInfo as $result) {
-                        $resInfo2 = $flights->GetFlightWithFieldsFilter($result[1], null, null, null, null, null, null, null, null);
-                        $hasPaid = $customers->HasPaid($result[0]);
-                        if ($hasPaid) {
-                            foreach ($resInfo2 as $result2) {
-                                ?>
-                                <tr>
-                                    <td align="center"> <?= $result[1] ?> </td> <!-- Flight Code -->
-                                    <td align="center"> <?= $result2[1] ?> </td> <!-- Origin -->
-                                    <td align="center"> <?= $result2[2] ?> </td>	<!-- Departure Date & Time -->
-                                    <td align="center"> <?= $result[7] ?> </td> <!-- Seat Number -->
-                                    <td align="center"> <?= $result[3] ?> </td> <!-- Name -->
-                                    <td align="center"> <?= $result[4] ?> </td> <!-- NRIC -->
-                                    <td align="center"> <?= $result[6] ?> </td> <!-- Contact Number -->
-                                    <td align="center"> <?= $result[5] ?> </td> <!-- Email Address -->
-                                </tr>
-                                <?php
+                $database = new Database('root', '', 'sqm_fp');
+                $locations = new Locations($database);
+                $flights = new Flights($database);
+                $customers = new Customers($database);
+                $res = new Reservations($database);
+                ?>
+
+                <!-- Form -->
+                <div id="form-wrapper">
+                    <form class="horizontal-form" role="form" name="myForm" action="retrieveFlight.php" onsubmit="javascript:return validate();" method="post">
+                        <div class="form-group">
+                            <label class="col-sm-4 control-label">Reservation Code: &nbsp </label>
+                            <div class="col-sm-4">
+                                <input class="form-control" type="text" name="code"> &nbsp
+                            </div> 
+                        </div>
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                    </form>
+                </div>
+
+
+
+                <?php
+                $reservation_code = (isset($_POST['code']) ? $_POST['code'] : null);
+                if ($reservation_code == null) {
+                    ?> 
+                    <tr> 
+                        <td  colspan ="8" align="center">Flight information will be diplayed here.</td>
+                    </tr>             
+                    <?php } else {
+                    ?>
+                    <table border="1" class="table table-bordered">
+
+                        <!-- Flight Code -->
+                        <tr>
+                            <th align="center">Flight code</th>
+                            <th align="center">Origin</th>
+                            <th align="center">Departure Date & Time</th>
+                            <th align="center">Seat Number </th>
+                            <th align="center">Name</th>
+                            <th align="center">NRIC</th>
+                            <th align="center">Contact Number</th>
+                            <th align="center">Email Address</th>
+
+                        </tr>
+                        <?php
+                        try {
+                            $resInfo = $res->GetReservationWithFieldsFilter($reservation_code, null, null, null, null, null, null, null, null, null);
+                            //$resInfo2 = $flights->GetFlightWithFieldsFilter($result[1], null, null, null, null, null, null, null, null);
+
+                            foreach ($resInfo as $result) {
+                                $resInfo2 = $flights->GetFlightWithFieldsFilter($result[1], null, null, null, null, null, null, null, null);
+                                $hasPaid = $customers->HasPaid($result[0]);
+                                if ($hasPaid) {
+                                    foreach ($resInfo2 as $result2) {
+                                        ?>
+                                        <tr>
+                                            <td align="center"> <?= $result[1] ?> </td> <!-- Flight Code -->
+                                            <td align="center"> <?= $result2[1] ?> </td> <!-- Origin -->
+                                            <td align="center"> <?= $result2[2] ?> </td>	<!-- Departure Date & Time -->
+                                            <td align="center"> <?= $result[7] ?> </td> <!-- Seat Number -->
+                                            <td align="center"> <?= $result[3] ?> </td> <!-- Name -->
+                                            <td align="center"> <?= $result[4] ?> </td> <!-- NRIC -->
+                                            <td align="center"> <?= $result[6] ?> </td> <!-- Contact Number -->
+                                            <td align="center"> <?= $result[5] ?> </td> <!-- Email Address -->
+                                        </tr>
+                                        <?php
+                                    }
+                                } else {
+                                    echo 'The flight you search is not paid yet. Please make payment first before checking the result<br>';
+                                    echo '<a href="../FrontEnd/purchaseFlight.php">Make Payment</a>';
+                                }
                             }
-                        }else{
-                            echo 'The flight you search is not paid yet. Please make payment first before checking the result<br>';
-                            echo '<a href="../FrontEnd/purchaseFlight.php">Make Payment</a>';
+                        } catch (Exception $e) {
+                            echo '<td colspan="8" align="center"> Invalid Reservation Code </td>';
                         }
                     }
-                } catch (Exception $e) {
-                    echo '<td colspan="8" align="center"> Invalid Reservation Code </td>';
-                }
-            }
-            ?>
+                    ?>
 
-        </table>
-        </div>
+                </table>
+            </div>
         </div>
 
     </body>
